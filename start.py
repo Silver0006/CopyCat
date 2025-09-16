d156305e54fe7a9946d9e645426f31cb7125a8b2
Silver0006 <52266188+Silver0006@users.noreply.github.com>
2025-09-16 16:17:34 -0400

    Finished proof of concept.

--- start.py
+++ start.py
@@ -4,6 +4,9 @@ import pyautogui, cv2, pytesseract
 Primary_Monitor_Resolution = pyautogui.size()
 App_Window_Names = pyautogui.getAllTitles()
 
+#Data
+Cord_to_click = []
+
 Search_Term = input("Type in text to search for: ")
 print("Resolution: " + str(Primary_Monitor_Resolution))
 Current_Image_Info = pyautogui.screenshot('Current_Image.png')
@@ -14,6 +17,16 @@ Image_Data = pytesseract.image_to_data(Current_Image_Grayscale, output_type=pyte
 for i in range(len(Image_Data['text'])):
     #print(Image_Data["text"][i] + " " + str(Image_Data["left"][i]) + " " + str(Image_Data["top"][i]))
     if Image_Data["text"][i] == Search_Term:
-        print(str(Image_Data["left"][i]) + " " + str(Image_Data["top"][i]))
-
+        Cord_to_click.append((Image_Data["left"][i],Image_Data["top"][i]))
+        print(str(Cord_to_click[0][0]) + " " + str(Cord_to_click[0][1]))
+if not Cord_to_click:
+    print("Text not found, canceling search")
+else:
+    while True:
+        try:
+            number_of_clicks:int = int(input("How many clicks?: "))
+            break
+        except ValueError:
+            print("Input a number.")
+    pyautogui.click(x=Cord_to_click[0][0], y=Cord_to_click[0][1],clicks=number_of_clicks)
 exit(0)
